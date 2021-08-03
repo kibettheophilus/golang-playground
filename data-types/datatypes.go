212c8c8da5868f961c039c9b8c509b9687ed35d0
kibet <kibettheophilus@gmail.com>
2021-08-03 12:01:07 +0300

    data types

---
+++ data-types/datatypes.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//NUMBERS,BOOLEANS AND STRINGS
+
+
+func main(){
+	//signed ints can hold zero,negative and positive values
+	 X := -22222
+	fmt.Println(X, X + 3, X - 3)
+
+	//unsigned ints can only hold zeros and positive values
+	var Y uint8 = 225
+	fmt.Println(Y, Y + 3, Y - 3)
+
+	//you can declare a datatype or leave for the compiler to decide
+}
